6e5f51e9bcd8d210aa347305f4881522c2737922
LegitSoulja <support@legitsoulja.info>
2018-05-14 18:19:49 -0400

    update

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+﻿requirejs({
+    baseUrl: '',
+    paths: {
+        scripts: 'assets/js/scripts',
+        THREE: 'assets/js/three.min'
+    }
+})
+
+
+requirejs(['scripts/InfiniteCreations'], function (InfiniteCreations) {
+
+    new InfiniteCreations();
+})
